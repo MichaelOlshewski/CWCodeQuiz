633e8021bdc656e94df27c2896e0ccf7d3c59060
Michael Olshewski <michael.olshewski@yahoo.com>
2020-06-16 23:57:06 -0400

    Added JS Logic

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,12 @@
-const startButton = document.querySelector("#startButton")
+const startButton = document.querySelector("#start-btn");
+const questionContainer = document.querySelector("#questionContainer");
+const mainHeader = document.querySelector("#mainHeader");
+const timerSpan = document.querySelector("#timerSpan");
+const nextButton = document.querySelector("#next-btn");
+const questionElement = document.querySelector("#question");
+const answerBtns = document.querySelector("#answerBtns");
+
+startButton.addEventListener("click", startQuiz);
 
 const questions = [
     {
@@ -37,18 +45,69 @@ const questions = [
     }
 ];
 
+let randomQuestions, currentQuestion;
+
 console.log(questions);
 
 // questionElement.textContent = JSON.stringify(questions[0].question);
 
 function startQuiz() {
-
+    mainHeader.classList.add('hide');
+    startButton.classList.add('hide');
+    randomQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestion = 0;
+    questionContainer.classList.remove('hide');
+    timerSpan.classList.remove('hide');
+    nextButton.classList.remove('hide');
+    nextQuestion();
 }
 
 function nextQuestion() {
-
+    resetState();
+    showQuestion(randomQuestions[currentQuestion]);
 }
 
-function chooseAnswer() {
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add("btn")
+        button.classList.add("answerBtn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', chooseAnswer);
+        answerBtns.appendChild(button);
+    })
+}
 
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerBtns.firstChild) {
+        answerBtns.removeChild(answerBtns.firstChild);
+    }
+}
+
+function chooseAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerBtns.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
