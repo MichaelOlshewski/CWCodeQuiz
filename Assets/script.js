3be1add6a6caf1d34e5f2862b4eab4e63ba2ba93
Michael Olshewski <michael.olshewski@yahoo.com>
2020-06-20 22:46:25 -0400

    Finished Assignment // Added README // Added Additional Functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,23 @@
-// Start and Next Button
+// Start, Next, Return Buttons
 const startButton = document.querySelector("#start-btn");
 const nextButton = document.querySelector("#next-btn");
+const returnStart = document.querySelector("#returnStart");
+const goToStart = document.querySelector("#goToStart");
 
 // Main Body Content
 const mainHeader = document.querySelector("#mainHeader");
 const quizContainer = document.querySelector("#quiz");
 const questionElement = document.querySelector("#question");
+const finalDiv = document.querySelector("#finalDiv");
+let finalScore = document.querySelector("#finalScore");
+const saveBtn = document.querySelector("#save");
+const initialInput = document.querySelector("#initialInput");
+const saved = document.querySelector("#saved");
+const error = document.querySelector("#error");
+const viewHighScores = document.querySelector("#viewHighScores");
+const showHighScores = document.querySelector("#showHighScores");
+const clearStorage = document.querySelector("#clearStorage");
+const highScoreTable = document.querySelector("#highScores");
 
 // Answer Buttons
 const choicesDiv = document.querySelector("#choices");
@@ -20,38 +32,42 @@ const isIncorrect = document.querySelector("#isIncorrect");
 
 //Timer Settings
 const timerSpan = document.querySelector("#timerSpan");
-const timeRemaining = document.querySelector("#timeRemaining");
+let timeRemaining = document.querySelector("#timeRemaining");
+var timerLength = "21" // Seconds + 1 to get XX seconds displayed
 
 // Event Listeners Go Here
 startButton.addEventListener("click", startQuiz);
-//startButton.addEventListener("click", startTimer);
 nextButton.addEventListener("click", nextQuestion);
-
+returnStart.addEventListener("click", returnToStart);
+saveBtn.addEventListener("click", saveToLocalStorage);
+viewHighScores.addEventListener("click", displayHighScores);
+goToStart.addEventListener("click", returnToStart);
+clearStorage.addEventListener("click", clearLocalStorage);
 
 // Questions Array
 let questions = [
     {
         question: 'How do you write a single line comment in JavaScript?',
-        choiceA: '//',
-        choiceB: '!!',
-        choiceC: '/* */',
-        choiceD: '/- -/',
+        choiceA: '1. //',
+        choiceB: '2. !!',
+        choiceC: '3. /* */',
+        choiceD: '4. /- -/',
         correct: "A"
     },
     {
         question: 'How do you write a multi line comment in JavaScript?',
-        choiceA: '//',
-        choiceB: '/--',
-        choiceC: '/* */',
-        choiceD: '<!-- -->',
+        choiceA: '1. //',
+        choiceB: '2. /--',
+        choiceC: '3. /* */',
+        choiceD: '4. /- -/',
         correct: 'C'
     },
     {
         question: 'What choice shows strict type checking?',
-        choiceA: '!=',
-        choiceB: '===',
-        choiceC: '=',
-        choiceD: '+=',
+        choiceA: '1. !=',
+        choiceB: '2. ===',
+        choiceC: '3. =',
+        choiceD: '4. +=',
         correct: 'B'
     }
 ];
@@ -60,6 +76,8 @@ let lastQuestionIndex = questions.length - 1;
 let runningQuestionIndex = 0;
 let score = 0;
 let timer;
+var seconds;
+var table = "";
 
 function renderQuestion() {
     let q = questions[runningQuestionIndex];
@@ -82,11 +100,12 @@ function answerIsWrong() {
 
 function startTimer() {
     timerSpan.classList.remove('hide');
-    var seconds = timeRemaining.textContent;
+    seconds = timeRemaining.textContent;
     seconds--;
     timeRemaining.textContent = seconds;
     if (seconds <= 0) {
         clearInterval(timer);
+        renderScore();
     }
 }
 
@@ -96,6 +115,9 @@ function checkAnswer(answer) {
         answerIsCorrect();
     } else {
         answerIsWrong();
+        console.log(seconds);
+        timeRemaining.textContent = seconds;
+        timeRemaining.textContent = timeRemaining.textContent - 10;
     }
     nextButton.classList.remove('hide')
 }
@@ -104,6 +126,7 @@ function startQuiz() {
     mainHeader.classList.add('hide');
     quizContainer.classList.remove('hide');
     startButton.classList.add('hide');
+    timeRemaining.textContent = timerLength;
     timer = setInterval(startTimer, 1000);
     startTimer();
     renderQuestion();
@@ -118,6 +141,100 @@ function nextQuestion() {
     } else {
         clearInterval(timer);
         timerSpan.classList.add('hide');
-        scoreRender();
+        renderScore();
     }
+}
+
+function renderScore() {
+    isCorrect.classList.add('hide');
+    isIncorrect.classList.add('hide');
+    quizContainer.classList.add('hide');
+    nextButton.classList.add('hide');
+    finalDiv.classList.remove('hide')
+    finalScore.innerHTML = score;
+}
+
+function returnToStart() {
+    isCorrect.classList.add('hide');
+    isIncorrect.classList.add('hide');
+    quizContainer.classList.add('hide');
+    nextButton.classList.add('hide');
+    finalDiv.classList.add('hide')
+    mainHeader.classList.remove('hide');
+    startButton.classList.remove('hide');
+    error.classList.add('hide');
+    saved.classList.add('hide');
+    showHighScores.classList.add('hide');
+    viewHighScores.classList.remove('hide');
+    timerSpan.classList.add('hide');
+    score = 0;
+    runningQuestionIndex = 0;
+    clearInterval(timer);
+    initialInput.value = "";
+}
+
+let highScores = [];
+
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    if (initialInput.value === "") {
+        error.classList.remove('hide');
+        saved.classList.add('hide');
+    } else {
+        error.classList.add('hide');
+        saved.classList.remove('hide');
+        let highScore = {
+            name: initialInput.value,
+            userScore: score,
+        }
+        highScores.push(highScore);
+        initialInput.value = "";
+        localStorage.setItem("HighScoreList", JSON.stringify(highScores));
+    }
+}
+
+var outputHTML = "";
+
+function displayHighScores(array) {
+    isCorrect.classList.add('hide');
+    isIncorrect.classList.add('hide');
+    quizContainer.classList.add('hide');
+    nextButton.classList.add('hide');
+    finalDiv.classList.add('hide')
+    mainHeader.classList.add('hide');
+    startButton.classList.add('hide');
+    error.classList.add('hide');
+    saved.classList.add('hide');
+    viewHighScores.classList.add('hide');
+    timerSpan.classList.add('hide');
+    showHighScores.classList.remove('hide')
+    clearInterval(timer); 
+    let getHighScores = JSON.parse(localStorage.getItem("HighScoreList"));
+    for (var i = 0; i < getHighScores.length; i++) {
+        var openScoreLine = document.createElement("tr");
+        var nameTableData = document.createElement("td");
+        var scoreTableData = document.createElement("td");
+        nameTableData.textContent = getHighScores[i].name;
+        scoreTableData.textContent = getHighScores[i].userScore;
+        openScoreLine.append(nameTableData);
+        openScoreLine.append(scoreTableData);
+        highScoreTable.append(openScoreLine);
+    }
+    console.log(getHighScores)
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+    isCorrect.classList.add('hide');
+    isIncorrect.classList.add('hide');
+    quizContainer.classList.add('hide');
+    nextButton.classList.add('hide');
+    finalDiv.classList.add('hide')
+    mainHeader.classList.remove('hide');
+    startButton.classList.remove('hide');
+    error.classList.add('hide');
+    saved.classList.add('hide');
+    viewHighScores.classList.remove('hide');
+    timerSpan.classList.add('hide');
+    showHighScores.classList.add('hide')
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CWCodeQuiz
+# __*JavaScript Code Quiz*__
+
+## __Details About This Assignment__
+This homework was about putting together everything that we learned the past 5+ weeks, HTML, CSS and JavaScript into one application. I had to develop the HTML Structure, CSS Styling and JavaScript Logic by hand. The criteria seemed simple but this was definitely a difficult task. There are currently 3 questions added, and more can be added through the questions array. Timer length can be changed with a single line in the JavaScript code. Each answer that is wrong, must deduct 10 seconds, if that time goes below 0, it takes you to the score screen where you can save your score and name to local storage. The whole application was made with a single index.html page, and using DOM Manipulation to edit the page accordingly to what is being press on screen.
+
+### __Link to Live Website__
+https://michaelolshewski.github.io/CWCodeQuiz/
+
+### __Image of 'index.html'__
+![index.html](/Assets/images/indexImage.png)
